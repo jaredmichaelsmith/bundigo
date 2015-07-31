56032b3e862aaca2e7f68d97a181699013849509
jaredmichaelsmith <jared@jaredsmith.io>
2015-07-31 15:15:46 -0400

    Add main application code

---
+++ bundigo/__main__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+from .bundigo import main
+main()
---
+++ bundigo/bundigo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.0'
+
+
+import sys
+
+
+def main():
+    print('Executing bundigo version %s.' % __version__)
+    print('List of argument strings: %s' % sys.argv[1:])
